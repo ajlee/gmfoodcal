0d3b04caad92b0d6b86e2af79244acda4d30dbf7
alex <alex+webwork@amanita.pw>
2019-07-15 11:43:29 +0000

    moved to js function

--- web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
+++ web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
@@ -6,13 +6,16 @@
         var title_selector = "edit-field-event-dates-0-inline-entity-form-title-0-value";
 	var main_title_selector = "edit-title-0-value";
         var inline_form_selector = 'edit-field-event-dates-wrapper';
-	$("[data-drupal-selector='" + date_selector + "']").on('change',function() {
-		console.log('test');
-		console.log($("[data-drupal-selector='" + date_selector + "']").val());
-		var date_val = ($("[data-drupal-selector='" + date_selector + "']").val());
-		var main_title_val = $("[data-drupal-selector='" + main_title_selector + "'").val();
-		$("[data-drupal-selector='" + title_selector + "']").val(date_val + ' ' + main_title_val);
-	});
+	$("[data-drupal-selector='" + date_selector + "']").on('change', generateInlineNodeTitle);
+
+	function generateInlineNodeTitle(e) {
+	        console.log('test');
+                console.log($("[data-drupal-selector='" + date_selector + "']").val());
+                var date_val = ($("[data-drupal-selector='" + date_selector + "']").val());
+                var main_title_val = $("[data-drupal-selector='" + main_title_selector + "'").val();
+                $("[data-drupal-selector='" + title_selector + "']").val(date_val + ' ' + main_title_val);
+
+	}
     }
   };
 })(jQuery);
