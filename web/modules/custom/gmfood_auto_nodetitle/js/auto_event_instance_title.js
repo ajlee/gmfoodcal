b722cbd9a117204d68919c1bc2b0e2ca4a666538
alex <alex+webwork@amanita.pw>
2019-07-15 13:26:07 +0000

    working!

--- web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
+++ web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
@@ -8,23 +8,32 @@
         var inline_form_selector = 'edit-field-event-dates-wrapper';
 	$("[data-drupal-selector='" + date_selector + "']").on('change', generateInlineNodeTitle);
 
+	jQuery("[data-drupal-selector='" + main_title_selector + "']").on('change', triggerGenerateAll);
 	jQuery('.field--name-field-inline-event-date').each(function (index, value) { 
-		console.log('test ' + index); 
+		jQuery(this).on('change', generateInlineNodeTitle);
+		/*
+		console.log('test1.' + index); 
 		console.log(jQuery(this).find('input').val());
 		var fieldset= jQuery(this).parentsUntil('fieldset');
 		console.log(fieldset);
 		var inline_event_title = fieldset.find('[data-drupal-selector^="edit-field-event-dates-"][data-drupal-selector$="-inline-entity-form-title-0-value"]');
 		inline_event_title.val('bleh');
+		*/
 	});
- 
-	function generateInlineNodeTitle(e) {
-	        console.log('test');
-                console.log($("[data-drupal-selector='" + date_selector + "']").val());
-                var date_val = ($("[data-drupal-selector='" + date_selector + "']").val());
-                var main_title_val = $("[data-drupal-selector='" + main_title_selector + "'").val();
-                $("[data-drupal-selector='" + title_selector + "']").val(date_val + ' ' + main_title_val);
-
+	
+	function triggerGenerateAll() {
+		 jQuery('.field--name-field-inline-event-date').each(function (index, value) {
+			jQuery(this).trigger("change");
+		});
 	}
+	function generateInlineNodeTitle(e) {
+		var fieldset = $(this).parentsUntil('fieldset');
+                var date_val = ($(this).find('input').first().val());
+		var inline_title = fieldset.find('[data-drupal-selector^="edit-field-event-dates-"][data-drupal-selector$="-inline-entity-form-title-0-value"]');
+                var main_title_val = $("[data-drupal-selector='" + main_title_selector + "'").val();
+                $(inline_title).val(date_val + ' ' + main_title_val);
+	}
+	triggerGenerateAll();
     }
   };
 })(jQuery);
