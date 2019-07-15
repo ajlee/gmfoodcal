1619b2d219823ebcdb1e72b91e7c0674daa305d2
alex <alex+webwork@amanita.pw>
2019-07-15 11:38:42 +0000

    title includes node title

--- web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
+++ web/modules/custom/gmfood_auto_nodetitle/js/auto_event_instance_title.js
@@ -4,12 +4,14 @@
     attach: function (context) {
 	var date_selector = "edit-field-event-dates-0-inline-entity-form-field-event-date-0-value-date";
         var title_selector = "edit-field-event-dates-0-inline-entity-form-title-0-value";
+	var main_title_selector = "edit-title-0-value";
         var inline_form_selector = 'edit-field-event-dates-wrapper';
 	$("[data-drupal-selector='" + date_selector + "']").on('change',function() {
 		console.log('test');
 		console.log($("[data-drupal-selector='" + date_selector + "']").val());
 		var date_val = ($("[data-drupal-selector='" + date_selector + "']").val());
-		$("[data-drupal-selector='" + title_selector + "']").val(date_val);
+		var main_title_val = $("[data-drupal-selector='" + main_title_selector + "'").val();
+		$("[data-drupal-selector='" + title_selector + "']").val(date_val + ' ' + main_title_val);
 	});
     }
   };
