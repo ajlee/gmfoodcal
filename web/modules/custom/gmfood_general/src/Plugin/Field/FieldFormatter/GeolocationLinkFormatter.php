5965561691ed0c730f2ad04c31656f6cd9ce162f
alex <alex+webwork@amanita.pw>
2019-07-22 12:34:39 +0000

    add location formatter

---
+++ web/modules/custom/gmfood_general/src/Plugin/Field/FieldFormatter/GeolocationLinkFormatter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\gmfood_general\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'geolocation_link_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "geolocation_link_formatter",
+ *   label = @Translation("Link to map"),
+ *   field_types = {
+ *     "geolocation"
+ *   }
+ * )
+ */
+class GeolocationLinkFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Displays a link to the map.');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+
+    foreach ($items as $delta => $item) {
+      // Render each element as markup.
+      $element[$delta] = ['#markup' => "<a href='#'>View in Google Maps</a>"];
+    }
+
+    return $element;
+  }
+
+}
