ff32082097c0061bf2ac967e89de27682e099c87
alex <alex+webwork@amanita.pw>
2019-08-11 19:46:02 +0000

    form mod email tested

--- web/modules/custom/gmfood_forms/src/Form/ModeratorAccessForm.php
+++ web/modules/custom/gmfood_forms/src/Form/ModeratorAccessForm.php
@@ -4,6 +4,8 @@ namespace Drupal\gmfood_forms\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
 
 class ModeratorAccessForm extends FormBase {
 
@@ -34,46 +36,61 @@ class ModeratorAccessForm extends FormBase {
      */
     public function buildForm(array $form, FormStateInterface $form_state) {
 
-      $form['description'] = [
-        '#type' => 'item',
-        '#markup' => $this->t('Please explain why you want moderator access to the site.'),
-      ];
+      $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+      //kint()
 
+      // find out if the user profile fields are complete
+      $user_picture = !$user->user_picture->isEmpty();
+      $field_about_me = !$user->field_about_me->isEmpty();
+      $field_public_name = !$user->field_public_name->isEmpty();
 
-  $form['title'] = [
-        '#type' => 'textarea',
-        '#title' => $this->t('Title'),
-        '#description' => $this->t('Please provide the reason you want access to moderate a calendar.'),
-        '#required' => TRUE,
-      ];
+      // check if the required form fields are completed
+      // these must be filled in before requesting moderator access
+      if ($field_about_me && $user_picture && $field_about_me) {
+            $form['description'] = [
+              '#type' => 'item',
+              '#markup' => $this->t('Please explain why you want moderator access to the site.'),
+            ];
+            $form['title'] = [
+              '#type' => 'textarea',
+              '#title' => $this->t('Title'),
+              '#description' => $this->t('Please provide the reason you want access to moderate a calendar.'),
+              '#required' => TRUE,
+            ];
+            $form['title'] = [
+                  '#type' => 'textarea',
+                  '#title' => $this->t('Calendars'),
+                  '#description' => $this->t('Please explain which calendar you want to moderate.'),
+                  '#required' => TRUE,
+                ];
+            $form['accept'] = array(
+              '#type' => 'checkbox',
+              '#title' => $this
+                ->t('I accept the terms of moderating this site'),
+              '#description' => $this->t('Please read and accept the terms of use'),
+            );
+            $form['actions'] = [
+              '#type' => 'actions',
+            ];
+            // Add a submit button that handles the submission of the form.
+            $form['actions']['submit'] = [
+              '#type' => 'submit',
+              '#value' => $this->t('Submit'),
+            ];
+      }
+      //
+      else {
+        $options = ['absolute' => TRUE];
+        $url = \Drupal\Core\Url::fromRoute('entity.user.edit_form', ['user' => \Drupal::currentUser()->id()], $options);
+        $url = $url->toString();
+        $form['description'] = [
+          '#type' => 'item',
+          '#markup' => $this->t('<h4>Please complete your account</h4>In order to be a moderator, you need to complete your profile. Please add a photo, description, and contact information, then you can request moderator access using this form. <ul><li><a href="' . $url . '">Edit your profile</a>'),
+        ];
+        \Drupal::messenger()->addError('You cannot request moderator access until you complete your profile.');
+        \Drupal::logger('gmfood_forms')->warning('access denied to moderator form.');
+      }
 
-      $form['title'] = [
-            '#type' => 'textarea',
-            '#title' => $this->t('Calendars'),
-            '#description' => $this->t('Please explain which calendar you want to moderate.'),
-            '#required' => TRUE,
-          ];
-
-      $form['accept'] = array(
-        '#type' => 'checkbox',
-        '#title' => $this
-          ->t('I accept the terms of moderating this site'),
-        '#description' => $this->t('Please read and accept the terms of use'),
-      );
-
-
-      // Group submit handlers in an actions element with a key of "actions" so
-      // that it gets styled correctly, and so that other modules may add actions
-      // to the form. This is not required, but is convention.
-      $form['actions'] = [
-        '#type' => 'actions',
-      ];
-
-      // Add a submit button that handles the submission of the form.
-      $form['actions']['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('Submit'),
-      ];
 
       return $form;
 
@@ -114,19 +131,54 @@ class ModeratorAccessForm extends FormBase {
         */
        public function submitForm(array &$form, FormStateInterface $form_state) {
 
-         // Display the results.
-
-         // Call the Static Service Container wrapper
-         // We should inject the messenger service, but its beyond the scope of this example.
          $messenger = \Drupal::messenger();
          $messenger->addMessage('Title: '.$form_state->getValue('title'));
          $messenger->addMessage('Accept: '.$form_state->getValue('accept'));
 
+
+         //
+         // send email to admin: provides a link for the admin to authorise the request for calendar moderator access
+         //
+         $mailManager = \Drupal::service('plugin.manager.mail');
+         $module = 'gmfood_forms';
+         $key = 'moderator_access_request';
+         $to = 'alexjameslee@gmail.com';
+         $langcode = 'en';
+         $params['email'] = 'alexjameslee@example.com';
+         $params['user'] = 'super user';
+         $params['phone'] = '07988 883764';
+         $params['message'] = 'dsf ohsdfoh  soihdsohso hodshf oihds oihfh';
+
+         // get a link to edit the user profile
+         $options = ['absolute' => TRUE];
+         $url = \Drupal\Core\Url::fromRoute('entity.user.edit_form', ['user' => \Drupal::currentUser()->id()], $options);
+         $url = $url->toString();
+         $params['url'] = $url;
+
+         // send it!
+         $send = TRUE;
+         $result = $mailManager->mail($module, $key, $to, $langcode, $params, null, $send);
+         var_dump($result);
+
+         // Was the email send successful?
+         if ($result['result'] !== true) {
+             drupal_set_message(t('There was a problem submitting your request.'), 'error');
+         } else {
+             drupal_set_message(t('Your message has been sent.'));
+         }
          // Redirect to home
          $form_state->setRedirect('<front>');
 
          // TODO: send email to admin
          // TODO: send email to user
 
+
+
+
+       }
+
+       public function access(AccountInterface $account) {
+        kint($account);
+        return AccessResult::allowedIf(TRUE);
        }
 }
